32ee19418838da0e6c9c7e0a4b9ec510d6204a43
kyuseok1 <jks980308@naver.com>
2024-09-12 00:56:23 +0900

    parallax 수정

--- src/app/components/ParallaxSection.tsx
+++ src/app/components/ParallaxSection.tsx
@@ -5,36 +5,20 @@ const ParallaxSection = () => {
   const { t } = useTranslation();
 
   return (
-    <Parallax className="parallax" bgImage="/images/home3.png" strength={700}>
-      <div className="relative">
-        <section className="flex flex-col justify-center items-center text-center p-12 md:p-24 bg-black bg-opacity-60 h-screen">
+    <Parallax
+      className="parallax"
+      bgImage="/images/home3.png"
+      strength={700}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <div className="relative" style={{ width: "100%", height: "100%" }}>
+        {" "}
+        <section className="flex flex-col justify-center items-center text-center p-12 md:p-24 bg-black bg-opacity-60 h-full w-full">
           <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-4">
             {t("코인의 모든것")}
           </h1>
           <p className="text-lg md:text-2xl text-gray-300 leading-relaxed">
-            {t("커뮤니티에서 쉽고 간편하게")}
-          </p>
-        </section>
-        <section className="flex flex-col justify-center items-center text-center p-12 md:p-24 bg-black bg-opacity-60 h-screen">
-          <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-4">
-            {t("코인 정보를 한곳에서 보고 ")}
-          </h1>
-          <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-4">
-            {t("한 곳에서 관리하세요.")}
-          </h1>
-          <h1 className="text-lg md:text-2xl text-gray-300 leading-relaxed">
-            {t("이제껏 경험 못했던 편리한 서비스")}
-          </h1>
-          <h1 className="text-lg md:text-2xl text-gray-300 leading-relaxed">
-            {t("커뮤니티와 함께라면 새로워질 거에요.")}
-          </h1>
-        </section>
-        <section className="flex flex-col justify-center items-center text-center p-12 md:p-24 bg-black bg-opacity-60 h-screen">
-          <p className="text-xl md:text-2xl text-gray-100 leading-relaxed">
-            {t("찾으시는 정보가 있으신가요?")}
-          </p>
-          <p className="text-xl md:text-2xl text-gray-100 leading-relaxed">
-            {t("누구나 쉽게 찾을수 있답니다.")}
+            {t(" 쉽고 간편하게")}
           </p>
         </section>
       </div>
